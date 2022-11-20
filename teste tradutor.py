f1a2d00d5085d021fd3818781d67ca5d2f702b20
mattnero003 <118364217+mattnero003@users.noreply.github.com>
2022-11-20 18:27:21 -0300

    Add files via upload

---
+++ teste tradutor.py
@@ -0,0 +1,295 @@
+a = ".-"
+b = "-..."
+c = "-.-."
+d = "-.."
+e = "."
+f = "..-."
+g = "--."
+h = "...."
+i = ".."
+j = ".---"
+k = "-.-"
+lm = ".-.."
+m = "--"
+n = "-."
+o = "---"
+p = ".--."
+q = "--.-"
+r = ".-."
+s = "..."
+t = "-"
+u = "..-"
+v = "...-"
+w = ".--"
+x = "-..-"
+y = "-.--"
+z = "--.."
+aa = "a"
+bb = "b"
+cc = "c"
+dd = "d"
+ee = 'e'
+ff = 'f'
+gg = 'g'
+hh = "h"
+ii = 'i'
+jj = 'j'
+kk = "k"
+ll = "l"
+mm = "m"
+nn = "n"
+oo = 'o'
+pp = "p"
+qq = "q"
+rr = "r"
+ss = "s"
+tt = "t"
+uu = "u"
+vv = "v"
+ww = "w"
+xx = "x"
+yy = "y"
+zz = "z"
+umm = "1"
+dois = "2"
+tres = "3"
+quatro = "4"
+cinco = "5"
+seis = "6"
+sete ="7"
+oito = "8"
+nove = "9"
+zero = "0"
+uum = ".----"
+do = "..---"
+tre = "...--"
+qua = "....-"
+cinq = "....."
+se = "-...."
+set = "--..."
+oi = "---.."
+no = '----.'
+de = '-----'
+parar = "Parar"
+
+texto = input("Digite o código ou letra: ")
+if texto == a:
+    print(aa)
+    texto = input("Digite o código ou letra: ")
+if texto == b:
+    print(bb)
+    texto = input("Digite o código ou letra: ")
+if texto == c:
+    print(cc)
+    texto = input("Digite o código ou letra: ")
+if texto == d:
+    print(dd)
+    texto = input("Digite o código ou letra: ")
+if texto == e:
+    print(ee)
+    texto = input("Digite o código ou letra: ")
+if texto == f:
+    print(ff)
+    texto = input("Digite o código ou letra: ")
+if texto == g:
+    print(gg)
+    texto = input("Digite o código ou letra: ")
+if texto == h:
+    print(hh)
+    texto = input("Digite o código ou letra: ")
+if texto == i:
+    print(ii)
+    texto = input("Digite o código ou letra: ")
+if texto == j:
+    print(jj)
+    texto = input("Digite o código ou letra: ")
+if texto == k:
+    print(kk)
+    texto = input("Digite o código ou letra: ")
+if texto == lm:
+    print(ll)
+    texto = input("Digite o código ou letra: ")
+if texto == m:
+    print(mm)
+    texto = input("Digite o código ou letra: ")
+if texto == n:
+    print(nn)
+    texto = input("Digite o código ou letra: ")
+if texto == o:
+    print(oo)
+    texto = input("Digite o código ou letra: ")
+if texto == p:
+    print(pp)
+    texto = input("Digite o código ou letra: ")
+if texto == q:
+    print(qq)
+    texto = input("Digite o código ou letra: ")
+if texto == r:
+    print(rr)
+    texto = input("Digite o código ou letra: ")
+if texto == s:
+    print(ss)
+    texto = input("Digite o código ou letra: ")
+if texto == t:
+    print(tt)
+    texto = input("Digite o código ou letra: ")
+if texto == u:
+    print(uu)
+    texto = input("Digite o código ou letra: ")
+if texto == v:
+    print(vv)
+    texto = input("Digite o código ou letra: ")
+if texto == w:
+    print(ww)
+    texto = input("Digite o código ou letra: ")
+if texto == x:
+    print(xx)
+    texto = input("Digite o código ou letra: ")
+if texto == y:
+    print(yy)
+    texto = input("Digite o código ou letra: ")
+if texto == z:
+    print(zz)
+    texto = input("Digite o código ou letra: ")
+if texto == aa:
+    print(a)
+    texto = input("Digite o código ou letra: ")
+
+if texto == bb:
+    print(b)
+    texto = input("Digite o código ou letra: ")
+if texto == cc:
+    print(c)
+    texto = input("Digite o código ou letra: ")
+if texto == dd:
+    print(d)
+    texto = input("Digite o código ou letra: ")
+if texto == ee:
+    print(e)
+    texto = input("Digite o código ou letra: ")
+if texto == ff:
+    print(ff)
+    texto = input("Digite o código ou letra: ")
+if texto == gg:
+    print(g)
+    texto = input("Digite o código ou letra: ")
+if texto == hh:
+    print(h)
+    texto = input("Digite o código ou letra: ")
+if texto == ii:
+    print(i)
+    texto = input("Digite o código ou letra: ")
+if texto == jj:
+    print(j)
+    texto = input("Digite o código ou letra: ")
+if texto == kk:
+    print(k)
+    texto = input("Digite o código ou letra: ")
+if texto == ll:
+    print(lm)
+    texto = input("Digite o código ou letra: ")
+if texto == mm:
+    print(m)
+    texto = input("Digite o código ou letra: ")
+if texto == nn:
+    print(n)
+    texto = input("Digite o código ou letra: ")
+if texto == oo:
+    print(o)
+    texto = input("Digite o código ou letra: ")
+if texto == pp:
+    print(p)
+    texto = input("Digite o código ou letra: ")
+if texto == qq:
+    print(q)
+    texto = input("Digite o código ou letra: ")
+if texto == rr:
+    print(r)
+    texto = input("Digite o código ou letra: ")
+if texto == ss:
+    print(s)
+    texto = input("Digite o código ou letra: ")
+if texto == tt:
+    print(t)
+    texto = input("Digite o código ou letra: ")
+if texto == uu:
+    print(u)
+    texto = input("Digite o código ou letra: ")
+if texto == vv:
+    print(v)
+    texto = input("Digite o código ou letra: ")
+if texto == ww:
+    print(w)
+    texto = input("Digite o código ou letra: ")
+if texto == xx:
+    print(x)
+    texto = input("Digite o código ou letra: ")
+if texto == yy:
+    print(y)
+    texto = input("Digite o código ou letra: ")
+if texto == zz:
+    print(z)
+    texto = input("Digite o código ou letra: ")
+
+if texto == umm:
+    print(uum)
+    texto = input("Digite o código ou letra: ")
+if texto == dois:
+    print(do)
+    texto = input("Digite o código ou letra: ")
+if texto == tres:
+    print(tre)
+    texto = input("Digite o código ou letra: ")
+if texto == quatro:
+    print(qua)
+    texto = input("Digite o código ou letra: ")
+if texto == cinco:
+    print(cinq)
+    texto = input("Digite o código ou letra: ")
+if texto == seis:
+    print(se)
+    texto = input("Digite o código ou letra: ")
+if texto == sete:
+    print(set)
+    texto = input("Digite o código ou letra: ")
+if texto == oito:
+    print(oi)
+    texto = input("Digite o código ou letra: ")
+if texto == nove:
+    print(no)
+    texto = input("Digite o código ou letra: ")
+if texto == zero:
+    print(de)
+    texto = input("Digite o código ou letra: ")
+if texto == uum:
+    print(umm)
+    texto = input("Digite o código ou letra: ")
+if texto == do:
+    print(dois)
+    texto = input("Digite o código ou letra: ")
+if texto == tre:
+    print(tres)
+    texto = input("Digite o código ou letra: ")
+if texto == qua:
+    print(quatro)
+    texto = input("Digite o código ou letra: ")
+if texto == cinq:
+    print(cinco)
+    texto = input("Digite o código ou letra: ")
+if texto == se:
+    print(seis)
+    texto = input("Digite o código ou letra: ")
+if texto == set:
+    print(sete)
+    texto = input("Digite o código ou letra: ")
+if texto == oi:
+    print(oito)
+    texto = input("Digite o código ou letra: ")
+if texto == no:
+    print(nove)
+    texto = input("Digite o código ou letra: ")
+if texto == de:
+    print(zero)
+    texto = input("Digite o código ou letra: ")
+if texto == parar:
+    print("Obrigado por usar o tradutor!")
